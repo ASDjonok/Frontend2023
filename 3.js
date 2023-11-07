f51bf7bb856df83f32edcc794f402e234bb80810
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2023-11-07 17:05:16 +0200

    add querySelector method calls

--- 3.js
+++ 3.js
@@ -1,2 +1,4 @@
-console.log("Hello world");
-console.log("!!!&&&", document.getElementsByTagName('h1'));
+// console.log("Hello world");
+console.log("document.getElementsByTagName('h1')", document.getElementsByTagName('h1'));
+console.log("document.querySelector('h1')", document.querySelector('h1'));
+//todo querySelectorAll
